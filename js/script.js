2b9d82d2e4d3fdcebdb35f9d55276f902c56ae52
M23 Gaming <infosando120@gmail.com>
2021-06-03 11:41:04 +0530

    Domain update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
-var releaseDate = new Date("May 30, 2021 08:30:00").getTime();
+/*
+Website designed and coded by Manikandan
+(c) copyright 2021
+*/
 
-var myfunc = setInterval(function() {
-    var current = new Date().getTime();
-    var timeLeft = releaseDate - current;
-
-    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("mins").innerHTML = minutes;
-    document.getElementById("secs").innerHTML = seconds;
-    
-    if(timeLeft < 0){
-    document.getElementById("days").innerHTML = "";
-    document.getElementById("hours").innerHTML = "";
-    document.getElementById("mins").innerHTML = "";
-    document.getElementById("secs").innerHTML = "";
-    }
-    }, 1000);
