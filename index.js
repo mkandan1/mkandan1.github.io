aa6a6cccaab2c834be4a47c52a4ae55789c4b4f6
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2022-01-20 23:54:11 +0530

    Version v0.03 with NodeJS

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+var url = require('url');
+const app = express();
+const port = 3000;
+
+// Set EJS as a Web Templating
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/assets', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/blog', (req, res) => {
+    res.render('blog');
+});
+
+app.get('/test', (req, res) => {
+    res.render('test');
+});
+
+// Set 404 Error page
+app.get('*',(req, res, next) => {
+    res.status(404).render('404');
+});
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Server booming at http://localhost:${port}`);
+});
